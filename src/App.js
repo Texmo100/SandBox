9a551d940fcc512696169cddc2314d3c4cc4ea8e
Texmo100 <isaac.ir@hotmail.com>
2021-04-30 16:24:31 -0700

    Todo list :3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React, {Component} from 'react'
-import Card from './Components/Card'
+import React, { Component } from 'react'
+import TodoItem from './Components/TodoItem'
+import todoList from './todosData'
 import './App.css';
 
 class App extends Component {
-  render(){
+  constructor(){
+    super()
+    this.state = {
+      todos: todoList
+    }
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+  
+  handleChange = (id) => {
+    this.setState( prevState => {
+      const newTodos = prevState.todos.map(todo => {
+        if(todo.id === id){
+          return{
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+
+      return{
+        todos: newTodos
+      }
+
+    })
+  }
+
+  render() {
+    const todoRender = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+
     return (
-      <div className="wrapper">
-        <Card name={'Toriis path 1'}/>
-        <Card name={'Toriis path 2'}/>
-        <Card name={'Toriis path 3'}/>
-        <Card name={'Toriis path 4'}/>
+      <div className="todo-list">
+        {todoRender}
       </div>
     );
   }
