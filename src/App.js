cf0cbb47ffc099c863a4d0cbc05d2bb6749aca72
Texmo100 <isaac.ir@hotmail.com>
2021-05-13 17:26:16 -0700

    Travel form version

--- src/App.js
+++ src/App.js
@@ -1,46 +1,83 @@
-import React, { Component } from 'react'
-import TodoItem from './Components/TodoItem'
-import todoList from './todosData'
-import './App.css';
+import React, { Component } from "react"
+import TravelForm from './Components/TravelForm'
+import './App.css'
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      todos: todoList
+      firstName: '',
+      lastName: '',
+      age: '',
+      gender: '',
+      destination: '',
+      dietaryRestrictions: {
+        isVegan: false,
+        isLactoseFree: false
+      }
     }
-    this.handleChange = this.handleChange.bind(this)
 
+    this.handleChange = this.handleChange.bind(this)
   }
-  
-  handleChange = (id) => {
-    this.setState( prevState => {
-      const newTodos = prevState.todos.map(todo => {
-        if(todo.id === id){
-          return{
-            ...todo,
-            completed: !todo.completed
+
+  //Handle change
+  handleChange = (event) => {
+    const { type, name, value, checked } = event.target
+
+    if (type === "checkbox") {
+      this.setState(prevState => {
+        return {
+          dietaryRestrictions: {
+            ...prevState.dietaryRestrictions,
+            [name]: checked
           }
         }
-        return todo
       })
+    } else {
+      this.setState({ [name]: value })
+    }
+  }
 
-      return{
-        todos: newTodos
-      }
-
-    })
+  //Handle submit
+  handleSubmit = (event) => {
+    alert("Info uploaded correctly")
   }
 
   render() {
-    const todoRender = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+    //Rendered words
+    const fullName = `${this.state.firstName} ${this.state.lastName}`
+    const ageRender = this.state.age === '' ? null : `${this.state.age} years old`
+    const isVeganRender = this.state.dietaryRestrictions.isVegan ? "yes" : "no"
+    const isLactoseFreRender = this.state.dietaryRestrictions.isLactoseFree ? "yes" : "no"
 
     return (
-      <div className="todo-list">
-        {todoRender}
-      </div>
-    );
+      <main className="wrapper">
+        <TravelForm 
+          firstName={this.state.firstName}
+          lastName={this.state.lastName}
+          age={this.state.age}
+          gender={this.state.gender}
+          destination={this.state.destination}
+          dietaryRestrictions={this.state.dietaryRestrictions}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        
+        {/* Info in real time */}
+        <div className="info">
+          <h2>Entered information:</h2>
+          <p>Your name: {fullName}</p>
+          <p>Your age: {ageRender}</p>
+          <p>Your gender: {this.state.gender}</p>
+          <p>Your destination: {this.state.destination}</p>
+          <p>Dietary Restrictions</p>
+          <p>is Lactose Free: {isLactoseFreRender}</p>
+          <p>is Vegan ?: {isVeganRender}</p>
+        </div>
+
+      </main>
+    )
   }
 }
 
-export default App;
+export default App
