2abdb4abb402ca4c29b19550c7330991f5d4f505
Texmo100 <isaac.ir@hotmail.com>
2021-10-19 13:42:12 -0700

    State added to the login form page

--- src/components/loginForm/Index.js
+++ src/components/loginForm/Index.js
@@ -1,30 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './loginForm.css'
 
 const Index = () => {
+    // useState Variables
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+
+    // input handler function
+    const inputHandler = event => {
+        const { value, name } = event.target
+
+        if(name === 'userName'){
+            setUserName(value)
+        }else if(name === 'password'){
+            setPassword(value)
+        }
+    }
+
+    // submit handler function
+    const submitHandler = event => {
+        event.preventDefault()
+        const userData = {
+            id: Math.random().toString(),
+            userName: userName,
+            password: password
+        }
+
+        console.log(userData)
+        inputCleaner()
+    }
+
+    // input cleaner function
+    const inputCleaner = () => {
+        setUserName('')
+        setPassword('')
+    }
+
     return (
         <div className='login-form'>
             <h1>Login</h1>
-            <form className='form'>
+            <form className='form' onSubmit={submitHandler}>
                 {/* label and input */}
                 <label className='form__label'>User name</label>
                 <input
                     className='form__input form__input--placeholder'
                     type='text'
-                    // name
-                    // value
+                    name='userName'
+                    value={userName}
                     placeholder='user name'
-                    // onChange
+                    onChange={inputHandler}
                 />
                 {/* label and input */}
                 <label className='form__label'>Password</label>
                 <input
                     className='form__input form__input--placeholder'
-                    type='text'
-                    // name
-                    // value
+                    type='password'
+                    name='password'
+                    value={password}
                     placeholder='password'
-                    // onChange
+                    onChange={inputHandler}
                 />
                 {/* submit */}
                 <input type='submit' value='submit' className='form__submit form__submit--hover'/>
