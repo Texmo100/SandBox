885467654647e3c2ce0de546968aea7c201cd29f
Texmo100 <isaac.ir@hotmail.com>
2021-12-23 11:59:54 -0700

    LogginMessage stylized, just a little bit

--- src/components/LogginMessage.js/LogginMessage.js
+++ src/components/LogginMessage.js/LogginMessage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import Modal from '../UI/Modal/Modal';
 import UserContext from '../../store/user-context';
+import styles from './LogginMessage.module.css';
 
 const LogginMessage = props => {
 
@@ -8,7 +9,10 @@ const LogginMessage = props => {
 
     return (
         <Modal onClick={props.onClose}>
-            {`Welcome ${userCtx.userName}`}
+            <div className={styles.message}>
+                {`Welcome ${userCtx.userName}`}
+                <button onClick={props.onClose} className={styles['message__button']}>Close modal</button>
+            </div>
         </Modal>
     );
 }
